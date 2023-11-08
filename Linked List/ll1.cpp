095e0bb71417e212ee0afec86bdb9fda8ed4f5d3
Rahul Mishra <rahulamishra1305@gmail.com>
2023-11-08 21:19:53 +0530

    update

--- Linked List/ll1.cpp
+++ Linked List/ll1.cpp
@@ -2,6 +2,64 @@
 #include <iostream>
 using namespace std;
 
-class LinkedList
+class Node
 {
+public:
+    int val;
+    Node *next;
+
+    Node(int data)
+    {
+        val = data;
+        next = NULL;
+    }
 };
+
+void insertAtHead(Node *&head, int val)
+{
+
+    Node *new_node = new Node(val);
+    new_node->next = head;
+    head = new_node;
+}
+
+void insertAtTail(Node *&head, int val)
+{
+    Node *new_node = new Node(val);
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = new_node;
+}
+
+void deleteAtHead(Node *&head)
+{
+
+    Node *temp = head;
+    head = head->next;
+    free(temp);
+}
+
+void display(node *head)
+{
+
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+int main()
+{
+
+    Node *head = NULL;
+    insertAthead(head, 2);
+    display(head);
+    insertAtHead(head, 1);
+    display(head);
+}
