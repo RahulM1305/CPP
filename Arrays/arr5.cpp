869618053bf1e6fa22ea38c21d6d6a82bb93b0db
Rahul Mishra <rahulamishra1305@gmail.com>
2024-03-05 22:43:03 +0530

    arrque

--- Arrays/arr5.cpp
+++ Arrays/arr5.cpp
@@ -1,1 +1,28 @@
-//
+// finding a unique number in given array
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int arr[] = {2, 3, 1, 3, 2, 4, 4, 9, 1};
+    int size = 9;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                arr[i] = arr[j] = -1;
+            }
+        }
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > 0)
+            cout << arr[i] << endl;
+    }
+
+    return 0;
+}
